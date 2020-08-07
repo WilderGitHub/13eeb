78425be3848bbbcd23748ef1d0d060939f85e55d
WilderGitHub <41117750+WilderGitHub@users.noreply.github.com>
2020-08-06 20:55:52 -0400

    Update carrusel.js

--- carrusel.js
+++ carrusel.js
@@ -48,5 +48,5 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 15000); // Change image every 2 seconds
 }
